f4b429c3388c8dcbe24d03b30af9b131dc5e5438
Joshfyn <joshuaadeegbe@yahoo.com>
2018-03-06 23:41:26 +0200

    still working on it

--- app/src/main/java/com/example/joshuaadeegbe/lab6/Main2Activity.java
+++ app/src/main/java/com/example/joshuaadeegbe/lab6/Main2Activity.java
@@ -1,29 +1,142 @@
 package com.example.joshuaadeegbe.lab6;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+import com.google.android.gms.vision.face.LargestFaceFocusingProcessor;
 
 public class Main2Activity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    private final int RC_PICTURE_TAKEN = 1111;
+    private ImageView photoTaken;
+    private FaceDetector faceDetector;
+    private CameraSource cameraSource;
+    private SurfaceHolder cameraHolder;
+    private final String ACTION_FACE_UPDATE = "ACTION_FACE_UPDATE";
+    public float faceSmile;
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main2);
+
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (photoTaken == null) {
+            Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(takePicture, RC_PICTURE_TAKEN);
+        }
+        faceDetector = new FaceDetector.Builder(getApplicationContext())
+                .setTrackingEnabled(true)
+                .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS) //eyes, smile
+                .build();
+
+        faceDetector.setProcessor(new LargestFaceFocusingProcessor(faceDetector, new FaceTracker()));
+        cameraSource = new CameraSource.Builder(getApplicationContext(), faceDetector)
+                .setFacing(CameraSource.CAMERA_FACING_FRONT)
+                .setRequestedFps(30)
+                .build();
+        /*try {
+            cameraSource.start(cameraHolder);
+            } catch (SecurityException e) {
+            e.printStackTrace();
+            } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+        faceDetector.release();
+
+
+
+    }
+    private class FaceTracker extends Tracker<Face> {
+        @Override
+        public void onMissing(Detector.Detections<Face> detections) {
+            super.onMissing(detections);
+
+
+        }
+
+        @Override
+        public void onUpdate(Detector.Detections<Face> detections, Face face) {
+            super.onUpdate(detections, face);
+            float smilepro = face.getIsSmilingProbability();
+            String smileprob = String.valueOf(smilepro);
+
+            Intent smiles = new Intent(ACTION_FACE_UPDATE);
+            smiles.putExtra("smiles", smileprob);
+            sendBroadcast(smiles);
+
+
+
+        }
+
+    }
+
+    private UIUpdater uiUpdater = new UIUpdater();
+    private class UIUpdater extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(ACTION_FACE_UPDATE)) {
+                String faceSmil = intent.getStringExtra("smiles");
+                faceSmile = Float.valueOf(faceSmil.trim()).floatValue();
+
             }
-        });
+
+        }
+    }
+
+    public void smile(){
+        Intent intent = new Intent(this, Main3Activity.class);
+        startActivity(intent);
+
+    }
+
+    public void notSmiling(){
+        Intent intent = new Intent(this, Main4Activity.class);
+        startActivity(intent);
+        Toast.makeText(getApplicationContext(), "Unfortunately, you are not smiling", Toast.LENGTH_SHORT).show();
+
+    }
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_PICTURE_TAKEN && resultCode == RESULT_OK) {
+            if (faceSmile >= 0.8){
+                smile();
+            }
+            else {
+                notSmiling();
+            }
+            
+
+
+        }
     }
 
 }
--- app/src/main/java/com/example/joshuaadeegbe/lab6/MainActivity.java
+++ app/src/main/java/com/example/joshuaadeegbe/lab6/MainActivity.java
@@ -1,46 +1,18 @@
 package com.example.joshuaadeegbe.lab6;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
+
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.pm.PackageManager;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.media.FaceDetector;
-import android.provider.MediaStore;
+
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.vision.CameraSource;
-import com.google.android.gms.vision.Detector;
-import com.google.android.gms.vision.Tracker;
-import com.google.android.gms.vision.face.Face;
-import com.google.android.gms.vision.face.LargestFaceFocusingProcessor;
-
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final int RC_PERMISSIONS = 2222;
-    private final String ACTION_FACE_UPDATE = "ACTION_FACE_UPDATE";
-
-    private SurfaceView cameraPreview;
-    private SurfaceHolder cameraHolder;
-
-    private CameraSource cameraSource;
-
-    private TextView debug_text;
+    private Button button;
 
     private com.google.android.gms.vision.face.FaceDetector faceDetector;
 
@@ -49,28 +21,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
-                ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
 
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, RC_PERMISSIONS);
-            finish();
-            return;
-        }
-
-        cameraPreview = findViewById(R.id.cameraview);
-        cameraHolder = cameraPreview.getHolder();
-        cameraHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-        cameraHolder.setFixedSize(Resources.getSystem().getDisplayMetrics().widthPixels, Resources.getSystem().getDisplayMetrics().heightPixels);
-
-        debug_text = findViewById(R.id.debug_text);
-
-        /*button = (Button) findViewById(R.id.button);
+        button = (Button) findViewById(R.id.btn_faces);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Main2Activity();
             }
-        });*/
+        });
 
     }
 
@@ -81,108 +39,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
-    private UIUpdater uiUpdater = new UIUpdater();
-    private class UIUpdater extends BroadcastReceiver {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if (intent.getAction().equals(ACTION_FACE_UPDATE)) {
-                debug_text.setText(intent.getStringExtra("faces"));
-            }
-
-            debug_text.invalidate();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(ACTION_FACE_UPDATE);
-        registerReceiver(uiUpdater, filter);
-
-        final Button faces = findViewById(R.id.btn_faces);
-        faces.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Initialising face detection...", Toast.LENGTH_SHORT).show();
-
-                if (cameraSource != null) cameraSource.stop();
-
-                if (faceDetector == null) {
-                    faceDetector = new com.google.android.gms.vision.face.FaceDetector.Builder(getApplicationContext())
-                            .setTrackingEnabled(true)
-                            .setClassificationType(com.google.android.gms.vision.face.FaceDetector.ALL_CLASSIFICATIONS) //eyes, smile
-                            .build();
-                }
-
-                if (faceDetector.isOperational()) {
-                    faceDetector.setProcessor(new LargestFaceFocusingProcessor(faceDetector, new FaceTracker()));
-                    cameraSource = new CameraSource.Builder(getApplicationContext(), faceDetector)
-                            .setFacing(CameraSource.CAMERA_FACING_FRONT)
-                            .setRequestedFps(30)
-                            .build();
-                    try {
-                        cameraSource.start(cameraHolder);
-                    } catch (SecurityException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                } else {
-                    Log.d("oops", "Face detection not supported by this camera....");
-                    Toast.makeText(getApplicationContext(), "Face detection not supported!", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-    }
-
-    private class FaceTracker extends Tracker<Face> {
-        @Override
-        public void onMissing(Detector.Detections<Face> detections) {
-            super.onMissing(detections);
-
-            Intent faces = new Intent(ACTION_FACE_UPDATE);
-            faces.putExtra("faces", detections.getDetectedItems().size());
-            sendBroadcast(faces);
-
-        }
-
-        @Override
-        public void onUpdate(Detector.Detections<Face> detections, Face face) {
-            super.onUpdate(detections, face);
-            if (face.getIsSmilingProbability()>=0){
-                Main2Activity();
-            }
-
-            String debug = "Faces: " + detections.getDetectedItems().size() + "Smile:" + (face.getIsSmilingProbability());
-
-            Intent faces = new Intent(ACTION_FACE_UPDATE);
-            faces.putExtra("faces", debug);
-            sendBroadcast(faces);
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        if (uiUpdater != null) unregisterReceiver(uiUpdater);
-        if (cameraSource != null) cameraSource.release();
-        if (faceDetector != null) faceDetector.release();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == RC_PERMISSIONS && resultCode == RESULT_OK) {
-            Intent reboot = new Intent(this, MainActivity.class);
-            startActivity(reboot);
-        }
-    }
 }
 
 
